54785b904e3ce199d4ef974a5ac3dd66774b2200
jahlgren <jukka.ahlgren@gmail.com>
2020-03-12 19:38:38 +0200

    Fix codeclimate and main test file.

--- src/test/java/ohtu/MainTest.java
+++ src/test/java/ohtu/MainTest.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package ohtu;
 
 /**
- *
+ * Tests for miniprojekti.
+ * 
  * @author jukka
  */
 public class MainTest {
